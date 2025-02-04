b4a8c7d89b4120d2c714075d1004b708cf88972f
gunz0blazin <168313608+gunz0blazin@users.noreply.github.com>
2025-02-04 10:40:40 -0500

    rename day one

---
+++ 100 Days of Python/Day1_bandNameGenerator.py
@@ -0,0 +1,13 @@
+#band name generator
+#welcome prompt
+print ("Welcome to name band generator!")
+
+#ask city name
+city = input ("What city did you grow up in? \n")
+
+#ask pet name
+pet = input ("What was the name of your first pet? \n")
+
+#Generate band name
+print ("Your band name could be " + city + " " + pet)
+
